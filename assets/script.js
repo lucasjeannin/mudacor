f2b7029fa157c46cfffa413a3e0af3e728efb230
lucas <lucasbjeannin@gmail.com>
2020-03-28 15:37:42 -0300

    separacao dos arquivos

---
+++ assets/script.js
@@ -0,0 +1,73 @@
+function mudacor() {
+    var caixa = document.getElementsByClassName("lucas1")[0];
+    switch(caixa.style.backgroundColor) {
+        case "":
+        case "blue":
+        caixa.style.backgroundColor = "black";
+        break;
+ 
+        case "black":
+        caixa.style.backgroundColor = "red";
+        break;
+ 
+        case "red":
+        caixa.style.backgroundColor = "blue";
+         break;
+    }
+  }
+ 
+  function mudacor2() {
+    var caixa = document.getElementsByClassName("lucas2")[0];
+    switch(caixa.style.backgroundColor) {
+        case "":
+        case "aqua":
+        caixa.style.backgroundColor = "chocolate";
+        break;
+ 
+        case "chocolate":
+        caixa.style.backgroundColor = "red";
+        break;
+ 
+        case "red":
+        caixa.style.backgroundColor = "green";
+        break;
+ 
+        case "green":
+        caixa.style.backgroundColor = "yellow";
+         break;
+ 
+ 
+         case "yellow":
+        caixa.style.backgroundColor = "white";
+         break;
+ 
+         case "white":
+         caixa.style.backgroundColor = "aqua";
+         break;
+    }
+  }
+ 
+ function mudacor3() {
+    var caixa = document.getElementsByClassName("lucas")[0];
+    switch(caixa.style.backgroundColor) {
+        case "":
+        case "red":
+        caixa.style.backgroundColor = "chocolate";
+        break;
+ 
+        case "chocolate":
+        caixa.style.backgroundColor = "blue";
+        break;
+ 
+        case "blue":
+        caixa.style.backgroundColor = "green";
+        break;
+ 
+        case "green":
+        caixa.style.backgroundColor = "yellow";
+         break;
+        case "yellow":
+        caixa.style.backgroundColor = "red";
+         break;
+    }
+  }
